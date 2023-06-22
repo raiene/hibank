23a1354600616a32f6649e8dd3e3543de0f12435
Raiene Pontes <raienesousa@mpto.mp.br>
2023-06-22 17:24:33 -0300

    Initial

---
+++ bin/hibank.dart
@@ -0,0 +1,5 @@
+import 'package:hibank/hibank.dart' as hibank;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hibank.calculate()}!');
+}
---
+++ test/hibank_test.dart
@@ -0,0 +1,8 @@
+import 'package:hibank/hibank.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
